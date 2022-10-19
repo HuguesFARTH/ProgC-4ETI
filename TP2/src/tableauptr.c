dc376030c339897e3ba525e2bc5da6a24bfa952b
Hugues <hugues.farthouat@cpe.fr>
2022-10-19 09:32:56 +0200

    bb

--- TP2/src/tableauptr.c
+++ TP2/src/tableauptr.c
@@ -12,28 +12,30 @@
 int main() {
     time_t t;
     srand((unsigned)time(&t));
-    int tableauInt[10] = {};
-    int *ptrInt = &tableauInt[0];
-    
-    float tableauFloat[10] = {};
-    float *ptrFloat = &tableauFloat[0];
-    
-    for (short i = 0; i < 10; i++) {
-        *ptrInt= rand();
-        *ptrFloat = rand()*0.0000033f;
-        printf("Int n°%hu: %d\n",i,*ptrInt);
-        printf("Float n°%hu: %f\n",i,*ptrFloat);
-        ptrInt+=1;
-        ptrFloat+=1;
-    }
-    printf("%d", tableauInt[1]);
-    
-    for (short i = 0; i< 10; i++){
-        printf("Int n°%hu: %d\n",i,*ptrInt);
-//        printf("Float n°%hu: %f\n",i,*ptrFloat);
-        ptrInt+=1;
-//        ptrFloat+=1;
-    }
+    int tableauInt[10];
+    int *ptrInt = &tableauInt;
 
+    float tableauFloat[10];
+    float *ptrFloat = &tableauFloat;
+
+    for (short i = 0; i < 10; i++) {
+        *(ptrInt+i)= rand()%100;
+        *(ptrFloat+i) = (rand()%100)*0.95f;
+        printf("Int n°%hu: %d\n",i,*(ptrInt+i));
+        printf("Float n°%hu: %f\n",i,*(ptrFloat+i));
+    }
+    //printf("%d", tableauInt[1]);
+    printf("----Modification----\n");
+    for (short i = 0; i < 10; i++){
+      if(i%2 == 0){
+        *(ptrInt+i) *= 3;
+        *(ptrFloat+i) *= 3;
+      }
+    }
+    for (short i = 0; i< 10; i++){
+      printf("Int n°%hu: %d\n",i,*(ptrInt+i));
+      printf("Float n°%hu: %f\n",i,*(ptrFloat+i));
+    }
+    printf("Fin du prgm\n");
     return 0;
 }
